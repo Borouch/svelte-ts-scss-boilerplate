f38b1159c74949af1c35e9c8aa02607604286ebd
Borouch <60584457+Borouch@users.noreply.github.com>
2022-12-17 15:57:44 +0200

    initial

--- src/App.svelte
+++ src/App.svelte
@@ -4,13 +4,16 @@
   let active = false;
 </script>
 
-<style>
-  .active {
-    color: red;
-  }
-</style>
 
 <h1 class:active>Svelte {name} version</h1>
 <button on:click={() => active = !active}>Toggle Active</button>
 
 <About />
+<style lang="scss">
+  .active {
+    color: red;
+    &:hover{
+      color:blue;
+    }
+  }
+</style>
